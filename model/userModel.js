04ba4ccb07eb3c396f07707c4b2fdbcdbfca89d8
Sachin Gunawardena <56478025+sachin-gun@users.noreply.github.com>
2024-12-27 22:49:10 +0530

    update user modal

--- model/userModel.js
+++ model/userModel.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 const { useDatabase } = require('..//config//db'); // Import the route_serviceDB connection
 
+
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String },
+    phone_number: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['ntc', 'bus_operator', 'commuter'], required: true },
-    phoneNumber: { type: String },
-    otp: { type: String },
-    otpExpiry: { type: Date },
-    lastLogin: { type: Date }
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
 });
 
-// Use the userServiceDB connection for the User model
 const userServiceDB = useDatabase('user_service')
 const User = userServiceDB.model('User', UserSchema);
+
 
 module.exports = User;
